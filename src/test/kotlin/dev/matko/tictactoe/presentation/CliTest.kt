87faa6f05fb65245ea1eb5dae7d143e14bea5d04
Matko Smoljan <matko.smoljan2@gmail.com>
2023-12-03 00:03:56 +0100

    refactor: refactor tests to use setUp()

--- src/test/kotlin/dev/matko/tictactoe/presentation/CliTest.kt
+++ src/test/kotlin/dev/matko/tictactoe/presentation/CliTest.kt
@@ -1,23 +1,32 @@
 package dev.matko.tictactoe.presentation
 
 import dev.matko.tictactoe.domain.Game
+import org.junit.jupiter.api.BeforeEach
 import java.io.ByteArrayOutputStream
+import java.io.OutputStream
 import java.io.PrintStream
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class CliTest {
 
+    private lateinit var cli: Cli
+    private lateinit var testOutputStream: OutputStream
+
+    @BeforeEach
+    fun setUp() {
+        this.testOutputStream = ByteArrayOutputStream()
+        this.cli = Cli(printStream = PrintStream(testOutputStream))
+    }
+
     @Test
     fun `Show empty board when the game starts`() {
-        val testOutputStream = ByteArrayOutputStream()
-        val cli = Cli(printStream = PrintStream(testOutputStream))
-        val victoryListener = Game.victoryListener(doOnGameChanged = { cli.printGame() })
+        val victoryListener = Game.victoryListener(doOnGameChanged = { this.cli.printGame() })
         val game = Game(victoryListener = victoryListener)
 
-        cli.useGame(game)
+        this.cli.useGame(game)
 
-        assertEquals("...\n...\n...\n", testOutputStream.toString())
+        assertEquals("...\n...\n...\n", this.testOutputStream.toString())
     }
 
 }
